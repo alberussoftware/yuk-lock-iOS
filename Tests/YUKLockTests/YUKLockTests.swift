3b0103395da438947f50c4247ffd585f7ae4a5a5
Ruslan Lutfullin <jeudesprits@icloud.com>
2021-01-30 20:04:24 +0500

    Added new version v1.0.0

--- Tests/YUKLockTests/YUKLockTests.swift
+++ Tests/YUKLockTests/YUKLockTests.swift
@@ -1,29 +1,79 @@
 import XCTest
 @testable import YUKLock
 
+// MARK: -
 final class YUKLockTests: XCTestCase {
-  // MARK:
+  // MARK: Private Props
   private var lock: UnfairLock!
   
-  // MARK:
+  // MARK: Public Static Props
+  static var allTests = [("testUnfairLock", testLockUnlock),
+                         ("testSync", testSync),
+                         ("testLocked", testLocked),
+                         ("testTrySync", testTrySync),
+                         ("testPrecondition", testPrecondition)]
+  
+  // MARK: Public Methods
+  override func setUp() {
+    super.setUp()
+    lock = UnfairLock()
+  }
+  //
+  func testLockUnlock() {
+    executeLockTest { (block) in
+      self.lock.lock()
+      block()
+      self.lock.unlock()
+    }
+  }
+  func testSync() {
+    executeLockTest { (block) in
+      self.lock.sync { block() }
+    }
+  }
+  func testLocked() {
+    lock.lock()
+    XCTAssertFalse(lock.locked())
+    lock.unlock()
+    XCTAssertTrue(lock.locked())
+    lock.unlock()
+  }
+  func testTrySync() {
+    lock.lock()
+    XCTAssertNil(lock.trySync({}))
+    lock.unlock()
+    XCTAssertNotNil(lock.trySync({}))
+  }
+  func testPrecondition() {
+    lock.lock()
+    lock.precondition(.onThreadOwner)
+    lock.unlock()
+    lock.precondition(.notOnThreadOwner)
+  }
+  //
+  override func tearDown() {
+    lock = nil
+    super.tearDown()
+  }
+  
+}
+
+extension YUKLockTests {
   private func executeLockTest(performBlock: @escaping (_ block: () -> Void) -> Void) {
     let dispatchBlockCount = 16
     let iterationCountPerBlock = 100_000
-    let queues: [DispatchQueue] = [
-      .global(qos: .userInteractive),
-      .global(),
-      .global(qos: .utility),
-    ]
+    let queues: [DispatchQueue] = [.global(qos: .userInteractive),
+                                   .global(),
+                                   .global(qos: .utility)]
     var value = 0
-    
     
     let group = DispatchGroup()
     
-    for block in 0..<dispatchBlockCount {
+    (0..<dispatchBlockCount).forEach {
       group.enter()
-      let queue = queues[block % queues.count]
+      let queue = queues[$0 % queues.count]
       queue.async {
-        for _ in 0..<iterationCountPerBlock {
+        (0..<iterationCountPerBlock).forEach { (_) in
           performBlock {
             value += 2
             value -= 1
@@ -35,66 +85,6 @@ final class YUKLockTests: XCTestCase {
     
     _ = group.wait(timeout: .distantFuture)
     
-    
     XCTAssertEqual(value, dispatchBlockCount * iterationCountPerBlock)
   }
-  
-  // MARK: -
-  // MARK:
-  override func setUp() {
-    super.setUp()
-    lock = UnfairLock()
-  }
-  
-  // MARK:
-  func testLockUnlock() {
-    executeLockTest { (block) in
-      self.lock.lock()
-      block()
-      self.lock.unlock()
-    }
-  }
-  
-  func testSync() {
-    executeLockTest { (block) in
-      self.lock.sync { block() }
-    }
-  }
-  
-  func testLocked() {
-    lock.lock()
-    XCTAssertFalse(lock.locked())
-    lock.unlock()
-    XCTAssertTrue(lock.locked())
-    lock.unlock()
-  }
-  
-  func testTrySync() {
-    lock.lock()
-    XCTAssertNil(lock.trySync({}))
-    lock.unlock()
-    XCTAssertNotNil(lock.trySync({}))
-  }
-  
-  func testPrecondition() {
-    lock.lock()
-    lock.precondition(condition: .onThreadOwner)
-    lock.unlock()
-    lock.precondition(condition: .notOnThreadOwner)
-  }
-  
-  // MARK:
-  override func tearDown() {
-    lock = nil
-    super.tearDown()
-  }
-  
-  // MARK:
-  static var allTests = [
-    ("testUnfairLock", testLockUnlock),
-    ("testSync", testSync),
-    ("testLocked", testLocked),
-    ("testTrySync", testTrySync),
-    ("testPrecondition", testPrecondition),
-  ] 
 }
